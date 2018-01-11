6f8cce40e0fa33269f2956725ea904fd222fbb76
wangyafei <wangyafei@startimes.com.cn>
2018-01-11 17:28:33 +0800

    modify webpack config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -331,7 +331,11 @@ module.exports = {
     new CopyWebpackPlugin([{
       from: path.join(__dirname, '../src/server/'),
       to:  path.join(__dirname, '../build/'),
-    }])
+    }]),
+    new webpack.ProvidePlugin({
+			$: "jquery",
+			jQuery: "jquery"
+		})
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
